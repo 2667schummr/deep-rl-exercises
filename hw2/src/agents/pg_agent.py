41bfa2d131723c7e8e90b951b60c5f1b182ebc3e
Ryan Schumm <ryan.schumm96@gmail.com>
2026-05-21 15:04:49 -0400

    Adds stability constant to normlization

--- hw2/src/agents/pg_agent.py
+++ hw2/src/agents/pg_agent.py
@@ -177,6 +177,6 @@ class PGAgent(nn.Module):
 
         # TODO: normalize the advantages to have a mean of zero and a standard deviation of one within the batch
         if self.normalize_advantages:
-            advantages = (advantages - np.mean(advantages)) / np.std(advantages)
+            advantages = (advantages - np.mean(advantages)) / (np.std(advantages) + 1e-8)
 
         return advantages
