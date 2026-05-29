f7ac3841559833c3ef2c0badf32dfd2dcaa943a8
Ryan Schumm <ryan.schumm96@gmail.com>
2026-05-29 15:30:06 -0400

    Implements clipped double-Q

--- hw3/src/agents/sac_agent.py
+++ hw3/src/agents/sac_agent.py
@@ -167,7 +167,7 @@ class SoftActorCritic(nn.Module):
         if self.target_critic_backup_type == "mean":
             next_qs = next_qs.mean(dim=0)
         elif self.target_critic_backup_type == "min":
-            next_qs = None
+            next_qs = next_qs.amin(dim=0)
         else:
             raise ValueError(
                 f"Invalid critic backup strategy {self.target_critic_backup_type}"
